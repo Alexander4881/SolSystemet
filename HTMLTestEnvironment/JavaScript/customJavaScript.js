b2e5b1d0c8dcfe65b4a2230adc7a93902c60bae9
Alexander L Mikkelsen <alihnmikkelsen@gmail.com>
2019-08-26 13:29:46 +0200

    upload

--- HTMLTestEnvironment/JavaScript/customJavaScript.js
+++ HTMLTestEnvironment/JavaScript/customJavaScript.js
@@ -6,6 +6,8 @@ var earth;
 const scene = new THREE.Scene();
 var tween;
 var temp = false;
+var curveSeaRoute;
+
 
 function main() {
   const canvas = document.querySelector('#c');
@@ -14,9 +16,9 @@ function main() {
   const fov = 45;
   const aspect = 2;  // the canvas default
   const near = 0.1;
-  const far = 100;
+  const far = 300;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
+  camera.position.set(0, 10, 150);
 
   const controls = new THREE.OrbitControls(camera, canvas);
   controls.target.set(0, 5, 0);
@@ -24,26 +26,26 @@ function main() {
 
   scene.background = new THREE.Color('black');
 
-  {
-    const planeSize = 40;
+  // {
+  //   const planeSize = 40;
 
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('../CSS/Objects/checker.png');
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.magFilter = THREE.NearestFilter;
-    const repeats = planeSize / 2;
-    texture.repeat.set(repeats, repeats);
+  //   const loader = new THREE.TextureLoader();
+  //   const texture = loader.load('../CSS/Objects/checker.png');
+  //   texture.wrapS = THREE.RepeatWrapping;
+  //   texture.wrapT = THREE.RepeatWrapping;
+  //   texture.magFilter = THREE.NearestFilter;
+  //   const repeats = planeSize / 2;
+  //   texture.repeat.set(repeats, repeats);
 
-    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshPhongMaterial({
-      map: texture,
-      side: THREE.DoubleSide,
-    });
-    const mesh = new THREE.Mesh(planeGeo, planeMat);
-    mesh.rotation.x = Math.PI * -.5;
-    scene.add(mesh);
-  }
+  //   const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+  //   const planeMat = new THREE.MeshPhongMaterial({
+  //     map: texture,
+  //     side: THREE.DoubleSide,
+  //   });
+  //   const mesh = new THREE.Mesh(planeGeo, planeMat);
+  //   mesh.rotation.x = Math.PI * -.5;
+  //   scene.add(mesh);
+  // }
 
   {
     const skyColor = 0xB1E1FF;  // light blue
@@ -63,18 +65,28 @@ function main() {
   }
 
   {
-    const objLoader = new THREE.OBJLoader2();
-    objLoader.loadMtl('../CSS/3D_Objects/test-earth.mtl', null, (materials) => {
-      // materials.Material.side = THREE.DoubleSide;
-      objLoader.setMaterials(materials);
-      objLoader.load('../CSS/3D_Objects/test-earth.obj', (event) => {
-        const root = event.detail.loaderRootNode;
-        earth = root;
-        scene.add(earth);
-      });
+    const gltfLoader = new THREE.GLTFLoader();
+    gltfLoader.load('../CSS/3DObjects/Solarsystemv2.gltf', (gltf) => {
+      const root = gltf.scene;
+      earth = root;
+      scene.add(root);
     });
-  }
 
+    // const objLoader = new THREE.OBJLoader2();
+    // objLoader.loadMtl('../CSS/3D_Objects/test-earth.mtl', null, (materials) => {
+    //   // materials.Material.side = THREE.DoubleSide;
+    //   objLoader.setMaterials(materials);
+    //   objLoader.load('../CSS/3D_Objects/test-earth.obj', (event) => {
+    //     const root = event.detail.loaderRootNode;
+    //     earth = root;
+    //     scene.add(earth);
+    //   });
+    // });
+
+
+    curveSeaRoute = new THREE.CatmullRomCurve3([new THREE.Vector3(18, roadHeight, 31), new THREE.Vector3(24, roadHeight, 23), new THREE.Vector3(22, roadHeight, 20), new THREE.Vector3(14, roadHeight, 19), new THREE.Vector3(10, roadHeight, 20), new THREE.Vector3(8, roadHeight, 23), new THREE.Vector3(10, roadHeight, 31)]);
+  }
+  
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
@@ -86,6 +98,13 @@ function main() {
     return needResize;
   }
 
+
+
+
+
+
+
+  var t = 0;
   function render(time) {
 
     if (resizeRendererToDisplaySize(renderer)) {
